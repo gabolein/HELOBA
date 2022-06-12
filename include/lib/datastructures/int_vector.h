fde040ca86ecba13aad4a78ece70f57165926b09
Jonas Kuß <jonaskuss@protonmail.com>
2022-06-12 14:58:07 +0200

    Generic Vector nur includen, wenn INT_VECTOR ihn auch wirklich braucht.

--- include/lib/datastructures/int_vector.h
+++ include/lib/datastructures/int_vector.h
@@ -1,7 +1,7 @@
-#include "lib/datastructures/generic/generic_vector.h"
-
 #ifndef INT_VECTOR_H
 #define INT_VECTOR_H
+
+#include "lib/datastructures/generic/generic_vector.h"
 
 MAKE_SPECIFIC_VECTOR_HEADER(int, int)
 
