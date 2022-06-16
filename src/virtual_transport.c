67a1d996d26173e5df9689a723f84ffada2a970a
Jonas Kuß <jonaskuss@protonmail.com>
2022-06-16 19:32:55 +0200

    Bessere Fehlermeldungen

--- src/virtual_transport.c
+++ src/virtual_transport.c
@@ -3,6 +3,7 @@
 #include <arpa/inet.h>
 #include <asm-generic/socket.h>
 #include <assert.h>
+#include <errno.h>
 #include <net/ethernet.h>
 #include <net/if.h>
 #include <netinet/in.h>
@@ -31,28 +32,27 @@ struct in_addr get_frequency_multicast_addr(uint16_t frequency) {
   assert(frequency_is_valid(frequency));
 
   struct in_addr out;
-  snprintf(multicast_addr, IPADDR_MAX_LEN, MULTICAST_FMT, (frequency & 0xFF00) >> 8, (frequency & 0x00FF) >> 0);
+  snprintf(multicast_addr, IPADDR_MAX_LEN, MULTICAST_FMT,
+           (frequency & 0xFF00) >> 8, (frequency & 0x00FF) >> 0);
   inet_aton(multicast_addr, &out);
   return out;
 }
 
-struct ip_mreq mreq = {
-  .imr_interface = {
-    .s_addr = INADDR_ANY
-  },
-  .imr_multiaddr = {0}
-};
+struct ip_mreq mreq = {.imr_interface = {.s_addr = INADDR_ANY},
+                       .imr_multiaddr = {0}};
 
 bool virtual_change_frequency(uint16_t frequency) {
   assert(frequency_is_valid(frequency));
 
-  if (mreq.imr_multiaddr.s_addr == 0)
-    setsockopt(virt_fd,IPPROTO_IP,IP_DROP_MEMBERSHIP, &mreq, sizeof(mreq));
-  
+  if (mreq.imr_multiaddr.s_addr != 0)
+    setsockopt(virt_fd, IPPROTO_IP, IP_DROP_MEMBERSHIP, &mreq, sizeof(mreq));
+
   mreq.imr_multiaddr = get_frequency_multicast_addr(frequency);
 
-  if (setsockopt(virt_fd,IPPROTO_IP,IP_ADD_MEMBERSHIP, &mreq,sizeof(mreq)) < 0) {
-    fprintf(stderr, "Couldn't join multicast group for frequency %u", frequency);
+  if (setsockopt(virt_fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) <
+      0) {
+    fprintf(stderr, "Couldn't join multicast group for frequency %u",
+            frequency);
     // NOTE: muss hier direkt die socket geschlossen werden
     close(virt_fd);
     return false;
@@ -67,15 +67,15 @@ bool virtual_transport_initialize() {
     return false;
   }
 
-
-  if (setsockopt(virt_fd, IPPROTO_IP, IP_MULTICAST_LOOP, NULL, 0) < 0) {
+  if (setsockopt(virt_fd, IPPROTO_IP, IP_MULTICAST_LOOP, &(int){0},
+                 sizeof(int)) < 0) {
     fprintf(stderr, "Couldn't enable multicast on loopback interface!\n");
     close(virt_fd);
     return false;
   }
 
-  if (setsockopt(virt_fd, IPPROTO_IP, SO_REUSEADDR, NULL, 0)) {
-    fprintf(stderr, "Couldn't get kernel to immediately reuse address after program terrmination.\n");
+  if (setsockopt(virt_fd, IPPROTO_IP, SO_REUSEADDR, &(int){1}, sizeof(int))) {
+    fprintf(stderr, "Couldn't set SO_REUSEADDR for socket.\n");
     close(virt_fd);
     return false;
   }
@@ -87,7 +87,8 @@ bool virtual_transport_initialize() {
 
 bool virtual_send_packet(uint8_t *buffer, unsigned length) {
   if (send(virt_fd, buffer, length, 0) < 0) {
-    fprintf(stderr, "Couldn't send %u bytes to fd=%i", length, virt_fd);
+    fprintf(stderr, "Couldn't send %u bytes to fd=%i:\n", length, virt_fd);
+    fprintf(stderr, "%s\n", strerror(errno));
     return false;
   }
 
@@ -102,12 +103,16 @@ bool virtual_receive_packet(uint8_t *buffer, unsigned *length) {
   // NOTE: Timeout sollte wahrscheinlich kleiner sein
   switch (poll(&fds, 1, 100)) {
   case -1:
-    fprintf(stderr, "Couldn't poll fd=%i", virt_fd);
+    fprintf(stderr, "Couldn't poll fd=%i:", virt_fd);
+    fprintf(stderr, "%s\n", strerror(errno));
     return false;
   case 0:
     return false;
   default:
-    recv(virt_fd, buffer, *length, 0);
+    if (recv(virt_fd, buffer, *length, 0) < 0) {
+      fprintf(stderr, "Couldn't receive message:\n");
+      fprintf(stderr, "%s\n", strerror(errno));
+    }
     *length = buffer[0];
     return true;
   }
