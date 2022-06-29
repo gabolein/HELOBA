b1fc0536566bdd82a89b61789dbb4229127958e8
Jonas Kuß <jonaskuss@protonmail.com>
2022-06-29 09:30:11 +0200

    Message Handler Grundgerüst schreiben
    Für jede (erlaubte) Kombination aus Action und Type
    gibt es eine spezifische Handlerfunktion, die von
    einer generellen Handlerfunktion aufgerufen wird.

---
+++ include/src/protocol/message_handler.h
@@ -0,0 +1,8 @@
+#ifndef MESSAGE_HANDLER_H
+#define MESSAGE_HANDLER_H
+
+#include "src/protocol/message.h"
+
+bool handle_message(message_t *msg);
+
+#endif
---
+++ src/protocol/message_handler.c
@@ -0,0 +1,122 @@
+#include "src/protocol/message_handler.h"
+#include "src/protocol/message.h"
+#include <assert.h>
+
+typedef bool (*handler_f)(message_t *msg);
+
+bool handle_do_mute(message_t *msg);
+bool handle_dont_mute(message_t *msg);
+bool handle_do_update(message_t *msg);
+bool handle_do_swap(message_t *msg);
+bool handle_will_swap(message_t *msg);
+bool handle_wont_swap(message_t *msg);
+bool handle_do_report(message_t *msg);
+bool handle_will_report(message_t *msg);
+bool handle_do_transfer(message_t *msg);
+bool handle_will_transfer(message_t *msg);
+bool handle_do_find(message_t *msg);
+bool handle_will_find(message_t *msg);
+
+static handler_f message_handlers[MESSAGE_ACTION_COUNT][MESSAGE_TYPE_COUNT] = {
+    [DO][MUTE] = handle_do_mute,
+    [DONT][MUTE] = handle_dont_mute,
+    [DO][UPDATE] = handle_do_update,
+    [DO][SWAP] = handle_do_swap,
+    [WILL][SWAP] = handle_will_swap,
+    [WONT][SWAP] = handle_wont_swap,
+    [DO][REPORT] = handle_do_report,
+    [WILL][REPORT] = handle_will_report,
+    [DO][TRANSFER] = handle_do_transfer,
+    [WILL][TRANSFER] = handle_will_transfer,
+    [DO][FIND] = handle_do_find,
+    [WILL][FIND] = handle_will_find};
+
+bool handle_do_mute(message_t *msg) {
+  assert(message_action(msg) == DO);
+  assert(message_type(msg) == MUTE);
+
+  return true;
+}
+
+bool handle_dont_mute(message_t *msg) {
+  assert(message_action(msg) == DONT);
+  assert(message_type(msg) == MUTE);
+
+  return true;
+}
+
+bool handle_do_update(message_t *msg) {
+  assert(message_action(msg) == DO);
+  assert(message_type(msg) == UPDATE);
+
+  return true;
+}
+
+bool handle_do_swap(message_t *msg) {
+  assert(message_action(msg) == DO);
+  assert(message_type(msg) == SWAP);
+
+  return true;
+}
+
+bool handle_will_swap(message_t *msg) {
+  assert(message_action(msg) == WILL);
+  assert(message_type(msg) == SWAP);
+
+  return true;
+}
+
+bool handle_wont_swap(message_t *msg) {
+  assert(message_action(msg) == WONT);
+  assert(message_type(msg) == SWAP);
+
+  return true;
+}
+
+bool handle_do_report(message_t *msg) {
+  assert(message_action(msg) == DO);
+  assert(message_type(msg) == REPORT);
+
+  return true;
+}
+
+bool handle_will_report(message_t *msg) {
+  assert(message_action(msg) == WILL);
+  assert(message_type(msg) == REPORT);
+
+  return true;
+}
+
+bool handle_do_transfer(message_t *msg) {
+  assert(message_action(msg) == DO);
+  assert(message_type(msg) == TRANSFER);
+
+  return true;
+}
+
+bool handle_will_transfer(message_t *msg) {
+  assert(message_action(msg) == WILL);
+  assert(message_type(msg) == TRANSFER);
+
+  return true;
+}
+
+bool handle_do_find(message_t *msg) {
+  assert(message_action(msg) == DO);
+  assert(message_type(msg) == FIND);
+
+  return true;
+}
+
+bool handle_will_find(message_t *msg) {
+  assert(message_action(msg) == WILL);
+  assert(message_type(msg) == FIND);
+
+  return true;
+}
+
+bool handle_message(message_t *msg) {
+  assert(message_is_valid(msg));
+
+  return message_handlers[message_action(msg)][message_type(msg)](msg);
+}
