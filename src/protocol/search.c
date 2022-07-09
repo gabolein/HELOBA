16fed2084500b9f0f61bfcfcd893b351aa1547ab
gabo <gabo@tu-berlin.de>
2022-07-09 16:27:18 +0200

    Implement data structure to access information about searching

---
+++ src/protocol/search.c
@@ -0,0 +1,29 @@
+#include "src/protocol/search.h"
+
+static search_state_t global_search_state;
+
+int frequency_cmp(frequency_t a, frequency_t b){
+  return 0;
+}
+
+MAKE_SPECIFIC_VECTOR_SOURCE(frequency_t, search_frequencies);
+MAKE_SPECIFIC_PRIORITY_QUEUE_SOURCE(frequency_t, search_frequencies, frequency_cmp)
+
+void search_frequencies_queue_add(frequency_t freq){
+  search_frequencies_priority_queue_push(global_search_state.search_frequencies, freq);
+}
+
+void search_state_initialize(){
+  global_search_state.searching = false;
+  global_search_state.search_frequencies = search_frequencies_priority_queue_create();
+}
+
+bool search_concluded(){
+  global_search_state.searching = false;
+  return true;
+}
+
+routing_id_t get_to_find(){
+  assert(global_search_state.searching);
+  return global_search_state.to_find_id;
+}
