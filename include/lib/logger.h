10a51ef97f8ccf129f3dc00e9417a99438b4bc86
Jonas Kuß <jonaskuss@protonmail.com>
2022-07-15 16:45:04 +0200

    Logger aus anderem Projekt kopieren.

---
+++ include/lib/logger.h
@@ -0,0 +1,55 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+
+#define PANIC_LEVEL 0
+#define WARNING_LEVEL 1
+#define DEBUG_LEVEL 2
+
+#ifndef LOG_LEVEL
+#define LOG_LEVEL DEBUG_LEVEL
+#endif
+
+#ifndef LOG_LABEL
+#define LOG_LABEL "???"
+#endif
+
+#if LOG_LEVEL >= DEBUG_LEVEL
+#define dbgln(...)                                                             \
+  do {                                                                         \
+    printf("(" LOG_LABEL ") ");                                                \
+    printf(__VA_ARGS__);                                                       \
+    printf("\n");                                                              \
+  } while (0)
+#else
+#define dbgln(...)
+#endif
+
+#if LOG_LEVEL >= WARNING_LEVEL
+#define warnln(...)                                                            \
+  do {                                                                         \
+    printf("[WARNING] (" LOG_LABEL ") ");                                      \
+    printf(__VA_ARGS__);                                                       \
+    printf("\n");                                                              \
+  } while (0)
+#else
+#define warnln(...)
+#endif
+
+#if LOG_LEVEL >= PANIC_LEVEL
+#define panicln(...)                                                           \
+  do {                                                                         \
+    printf("[PANIC] (" LOG_LABEL ") ");                                        \
+    printf(__VA_ARGS__);                                                       \
+    printf("\n");                                                              \
+    exit(0);                                                                   \
+  } while (0)
+#else
+#define panic(...)
+#endif
+
+#endif
