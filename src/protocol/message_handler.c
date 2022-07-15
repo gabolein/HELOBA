9c154b3cfe7eda0939c3200f76f3e7c6d04fcb4d
Jonas Kuß <jonaskuss@protonmail.com>
2022-07-15 12:55:38 +0200

    Erste Iteration des Election Algorithmus implementieren

--- src/protocol/message_handler.c
+++ src/protocol/message_handler.c
@@ -215,6 +215,17 @@ bool handle_do_migrate(message_t *msg) {
   return transport_change_frequency(destination);
 }
 
+bool election_filter(message_t *msg) {
+  return msg->header.sender_id.layer & leader ||
+         (message_action(msg) == WILL && message_type(msg) == TRANSFER);
+}
+
+bool join_filter(message_t *msg) {
+  return msg->header.sender_id.layer & leader &&
+         ((message_action(msg) == DO || message_action(msg) == DONT) &&
+          message_type(msg) == TRANSFER);
+}
+
 bool handle_do_transfer(message_t *msg) {
   assert(message_action(msg) == DO);
   assert(message_type(msg) == TRANSFER);
@@ -227,13 +238,88 @@ bool handle_do_transfer(message_t *msg) {
   frequency_t destination = msg->payload.transfer.to;
   transport_change_frequency(destination);
 
+  // clang-format off
+  // Leader election algorithm:
+  // 1. listen for random time N \in [MIN, MAX]
+  // 2. if someone else is sending on frequency, goto 5
+  // 3. send WILL TRANSFER immediately
+  // 4. receive all incoming messages
+  // 4.1. if leader answered, go to 6
+  // 4.2. if non-leader also sent WILL TRANSFER, goto 1
+  // 4.3. else elect yourself as leader
+  // 5. Timeout for time T or until leader message received (until we're sure election is finished)
+  // 6. if not already registered, send WILL TRANSFER
+  // clang-format on
+
+  bool participating = true;
+  bool leader_detected = false;
+  message_vector_t *received = message_vector_create();
+
   message_t join_request = message_create(WILL, TRANSFER);
   join_request.payload.transfer = (transfer_payload_t){.to = destination};
-
   routing_id_t receiver = {.layer = leader};
-  transport_send_message(&join_request, receiver);
 
-  return true;
+  while (participating) {
+    size_t listen_ms = random_number_between(0, 50);
+    struct timespec start_time;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &start_time);
+    while (!hit_timeout(listen_ms, &start_time) && participating) {
+      if (transport_channel_active()) {
+        participating = false;
+      }
+    }
+
+    if (participating) {
+      transport_send_message(&join_request, receiver);
+      message_assign_collector(50, UINT_MAX, election_filter, received);
+
+      if (message_vector_size(received) == 0) {
+        gs.flags |= LEADER;
+        gs.id.layer |= leader;
+        participating = false;
+      }
+
+      for (unsigned i = 0; i < message_vector_size(received) && participating;
+           i++) {
+        message_t current = message_vector_at(received, i);
+        if (current.header.sender_id.layer & leader) {
+          leader_detected = true;
+          participating = false;
+        }
+      }
+
+      message_vector_clear(received);
+    }
+  }
+
+  if (gs.flags & LEADER) {
+    return true;
+  }
+
+  if (!leader_detected) {
+    // NOTE: wir landen hier auch im Normalfall, wenn es schon einen Leader auf
+    // der Frequenz gibt. Wir sollten dann irgendwie einen Weg finden, nicht in
+    // diesem Timeout zu landen.
+    sleep_ms(500);
+  }
+
+  transport_send_message(&join_request, receiver);
+  // NOTE: ist es sicher, dass der received vector hier wieder leer ist?
+  message_assign_collector(50, 1, join_filter, received);
+
+  if (message_vector_size(received) == 0) {
+    fprintf(stderr, "Leader didn't answer join request.\n");
+    return false;
+  }
+
+  message_t answer = message_vector_at(received, 0);
+  if (message_action(&answer) == DONT) {
+    fprintf(stderr, "Leader rejected our join request.\n");
+    return false;
+  } else {
+    fprintf(stderr, "Leader accepted our join request.\n");
+    return true;
+  }
 }
 
 bool handle_will_transfer(message_t *msg) {
