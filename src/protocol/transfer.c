2111660a39ce84ec91c82023c18783b0a315b4d7
Jonas Kuß <jonaskuss@protonmail.com>
2022-07-16 16:24:31 +0200

    Formatting

--- src/protocol/transfer.c
+++ src/protocol/transfer.c
@@ -265,16 +265,18 @@ bool handle_will_transfer(message_t *msg) {
 
       club_hashmap_remove(gs.members, nonleader);
       gs.scores.current--;
-      dbgln("Will Transfer: Node is unregistering." 
-          " Membercount: %u", gs.scores.current);
+      dbgln("Will Transfer: Node is unregistering."
+            " Membercount: %u",
+            gs.scores.current);
     } else {
-      // NOTE because of election 
+      // NOTE because of election
       // we get more than one WILL TRANSFER from each node
       if (club_hashmap_exists(gs.members, nonleader)) {
         club_hashmap_insert(gs.members, nonleader, true);
         gs.scores.current++;
         dbgln("Will Transfer: Node is registering."
-            " Membercount: %u", gs.scores.current);
+              " Membercount: %u",
+              gs.scores.current);
       }
     }
   }
