bfeb637e9a2f8d6903bbf479019160fbdf3d0855
Jonas Kuß <jonaskuss@protonmail.com>
2022-07-17 16:24:03 +0200

    Alte Flag Initialisierung ersetzen

--- src/state.c
+++ src/state.c
@@ -7,7 +7,7 @@ MAKE_SPECIFIC_VECTOR_SOURCE(routing_id_t, routing_id_t)
 MAKE_SPECIFIC_HASHMAP_SOURCE(routing_id_t, bool, club, routing_id_equal)
 
 void initialize_global_state() {
-  gs.flags = 0;
+  gs.registered = false;
   score_state_t scores = {0, 0};
   gs.scores = scores;
   gs.members = club_hashmap_create();
