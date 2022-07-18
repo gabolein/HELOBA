1cb3b6e3a6e3bfa066a0e31e0ccd8f56c7054863
Jonas Kuß <jonaskuss@protonmail.com>
2022-07-18 15:27:22 +0200

    Duplizierten Test löschen

--- test/test_hashmap.c
+++ test/test_hashmap.c
@@ -142,14 +142,3 @@ Test(hashmap, destroy) {
   ii_hashmap_t *hm = ii_hashmap_create();
   ii_hashmap_destroy(hm);
 }
-
-Test(hashmap, infinite_loop, .timeout = 2) {
-  ii_hashmap_t *hm = ii_hashmap_create();
-  for (size_t i = 0; i < 10; i++) {
-    ii_hashmap_insert(hm, i, 42);
-    ii_hashmap_remove(hm, i);
-  }
-
-  ii_hashmap_exists(hm, 13);
-  ii_hashmap_destroy(hm);
-}
