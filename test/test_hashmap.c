a03c449f63a0af7da7c8b04c563a5de1e4f79b78
gabo <gabo@tu-berlin.de>
2022-07-18 15:04:23 +0200

    Rename infinite loop hashmap test

--- test/test_hashmap.c
+++ test/test_hashmap.c
@@ -131,7 +131,7 @@ Test(hashmap, destroy) {
   ii_hashmap_destroy(hm);
 }
 
-Test(hashmap, deleted, .timeout = 2) {
+Test(hashmap, infinite_loop, .timeout = 2) {
   ii_hashmap_t *hm = ii_hashmap_create();
   for (size_t i = 0; i < 10; i++) {
     ii_hashmap_insert(hm, i, 42);
