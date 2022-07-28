3c788a702d27b5adfe12e3407261f062c12211fd
Jonas Kuß <jonaskuss@protonmail.com>
2022-07-28 19:11:05 +0200

    rg durch grep ersetzen + andere kleine Verbesserungen
    So kann ich die Simulation auch auf meinem Laptop ausführen.

--- simulate_search.sh
+++ simulate_search.sh
@@ -1,22 +1,22 @@
 #!/bin/zsh
 
-nnodes=100
+nnodes=50
 simulation_runtime=60
 
 echo "Starting HELOBA Simulation with $nnodes Nodes running for $simulation_runtime seconds"
 
 rm search_log.txt
 make -s clean
-make -s simulation
+make -s build
 repeat $nnodes { ./build/heloba < /dev/null &>> search_log.txt& }
 
-nstarted=$(ps aux | rg heloba | wc -l)
+nstarted=$(pgrep heloba | wc -l)
 echo "Started $nstarted Nodes"
 
 sleep $simulation_runtime && pkill -SIGKILL heloba
 
-completed_searches=$(rg "Took" search_log.txt | wc -l)
-failed_searches=$(rg "find requested node" search_log.txt | wc -l)
+completed_searches=$(grep "Took" search_log.txt | wc -l)
+failed_searches=$(grep "find requested node" search_log.txt | wc -l)
 total_searches=$(echo "$completed_searches + $failed_searches" | bc)
 search_ratio=$(echo "scale=3 ; $completed_searches / $simulation_runtime" | bc)
 echo "Successfully Completed Searches: $completed_searches/$total_searches ($search_ratio per second)"
@@ -24,5 +24,5 @@ echo "Successfully Completed Searches: $completed_searches/$total_searches ($sea
 echo "Search Stats - Hops needed"
 grep -oP "Took\s+\K[0-9]+" search_log.txt | perl -M'List::Util qw(sum max min)' -MPOSIX -0777 -a -ne 'printf "%-7s : %d\n"x4, "Min", min(@F), "Max", max(@F), "Average", sum(@F)/@F,  "Median", sum( (sort {$a<=>$b} @F)[ int( $#F/2 ), ceil( $#F/2 ) ] )/2;'
 
-nswaps=$(rg "Sending DO SWAP" search_log.txt | wc -l)
+nswaps=$(grep "Sending DO SWAP" search_log.txt | wc -l)
 echo "Conducted $nswaps SWAPs"
